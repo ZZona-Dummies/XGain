49492e0eb35904b254dfe211964dd839d8f53e75
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-12 10:13:11 +0100

    Client property for passing Socket to event

--- Src/XGain/Message.cs
+++ Src/XGain/Message.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using XGain.Sockets;
 
 namespace XGain
 {
     public class Message : EventArgs
     {
+        public ISocket Client { get; set; }
         public object UserToken { get; set; }
         public byte[] RequestBytes { get; set; }
     }
