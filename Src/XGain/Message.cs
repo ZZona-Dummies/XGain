4725f309f8c0a7f2fd64433bcd4ccb54b917c582
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-12 10:11:56 +0100

    UserToken  instead of UserTokens

--- Src/XGain/Message.cs
+++ Src/XGain/Message.cs
@@ -4,7 +4,7 @@ namespace XGain
 {
     public class Message : EventArgs
     {
-        public object[] UserToken { get; set; }
+        public object UserToken { get; set; }
         public byte[] RequestBytes { get; set; }
     }
 }
