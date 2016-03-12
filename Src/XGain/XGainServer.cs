6dcaa1a4f77a6a4d4d9071fb4236f699bb43d5ca
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-12 09:44:04 +0100

    Rename XGainServer class

--- Src/XGain/XGainServer.cs
+++ Src/XGain/XGainServer.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
@@ -8,13 +7,13 @@ using XGain.Sockets;
 
 namespace XGain
 {
-    public class XGain : IServer
+    public class XGainServer : IServer
     {
         public event EventHandler<Message> OnNewMessage;
         private readonly Func<IProcessor> _requestProcessorResolver;
         private readonly TcpListener _listener;
 
-        public XGain(IPAddress ipAddress, int port, Func<IProcessor> requestProcessorResolver)
+        public XGainServer(IPAddress ipAddress, int port, Func<IProcessor> requestProcessorResolver)
         {
             _requestProcessorResolver = requestProcessorResolver;
             _listener = new TcpListener(ipAddress, port);
@@ -51,31 +50,7 @@ namespace XGain
             var handler = OnNewMessage;
             handler?.Invoke(socket, args);
         }
-
-        protected async void ProcessTcpCLientConnection(TcpClient client)
-        {
-            Message args = new Message();
-            using (MemoryStream ms = new MemoryStream())
-            {
-                using (NetworkStream ns = client.GetStream())
-                {
-                    while (ns.DataAvailable)
-                    {
-                        byte[] buffer = new byte[1024 * 8];
-
-                        int received = ns.Read(buffer, 0, buffer.Length);
-
-                        await ms.WriteAsync(buffer, 0, received);
-                    }
-
-                    args.RequestBytes = ms.ToArray();
-                }
-
-                var handler = OnNewMessage;
-                handler?.Invoke(client, args);
-            }
-        }
-
+        
         public void Dispose()
         {
             try
