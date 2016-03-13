95810121278ac0672b6e37debfdbd23a0ba70185
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-13 08:57:22 +0100

    IProcessor of T where T is Message

--- Src/XGain/Processing/IProcessor.cs
+++ Src/XGain/Processing/IProcessor.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using XGain.Sockets;
 
 namespace XGain.Processing
 {
-    public interface IProcessor<in T> where T : EventArgs
+    public interface IProcessor<in T> where T : Message
     {
         Task ProcessSocketConnection(ISocket socket, T args);
     }
