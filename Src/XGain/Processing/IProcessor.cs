2e0db44675e64b3ee176423173066ee17dd3d35f
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-13 09:50:02 +0100

    Non-generic IProcessor

--- Src/XGain/Processing/IProcessor.cs
+++ Src/XGain/Processing/IProcessor.cs
@@ -1,10 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using XGain.Sockets;
 
 namespace XGain.Processing
 {
-    public interface IProcessor<in T> where T : Message
+    public interface IProcessor<in T> : IProcessor where T : Message
     {
-        Task ProcessSocketConnection(ISocket socket, T args);
+        void ProcessSocketConnection(ISocket socket, T args);
+    }
+
+    public interface IProcessor
+    {
+        void ProcessSocketConnection(ISocket socket, EventArgs args);
     }
 }
