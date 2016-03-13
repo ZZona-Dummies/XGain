8d4389dfb3a5f70aa775429a9201b7470f30c90d
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-13 10:09:59 +0100

    Contravariance

--- Src/XGain/Processing/IProcessor.cs
+++ Src/XGain/Processing/IProcessor.cs
@@ -2,7 +2,7 @@
 
 namespace XGain.Processing
 {
-    public interface IProcessor<T> where T : Message
+    public interface IProcessor<in T> where T : Message
     {
         void ProcessSocketConnection(ISocket socket, T args);
     }
