e6c5c60d77736a4c10f8925cb2c8f0b5c3753935
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-15 17:45:52 +0100

    Missing StartParallel method in IServer

--- Src/XGain/IServer.cs
+++ Src/XGain/IServer.cs
@@ -7,6 +7,7 @@ namespace XGain
     public interface IServer : IDisposable
     {
         Task StartSynchronously(CancellationToken token);
+        Task StartParallel(CancellationToken token, int? maxDegreeOfParallelism = null);
         event EventHandler OnStart;
         event EventHandler<Message> OnNewMessage;
         event EventHandler<ErrorArgs> OnError;
