44683de5c381ce8ca6a703a5878f74856610e283
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-05-23 20:00:51 +0200

    Script for packaging

---
+++ Scripts/pack.ps1
@@ -0,0 +1,24 @@
+param (
+    [Parameter(Mandatory=$true)][string]$version = 0
+ )
+
+# projects to pack
+$projects = @(
+    "Src\XGain"
+);
+
+# pack function for project
+function Pack($path){
+    dotnet pack $path --version-suffix $version;
+}
+
+write-host "Packeging started";
+
+
+# build each project
+foreach ($project in $projects){
+    write-host "Packing " $project "with suffix version " $version
+    Pack($project);
+}
+
+write-host "Packeging finished";
