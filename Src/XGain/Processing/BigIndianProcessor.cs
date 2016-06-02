ea9dc57a724a77dc3fc22825c0ad222a57304a8e
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-06-02 20:12:35 +0200

    Added BigIndian processor

---
+++ Src/XGain/Processing/BigIndianProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using XGain.Sockets;
+
+namespace XGain.Processing
+{
+    public class BigIndianProcessor : IProcessor<MessageArgs>
+    {
+        public async Task ProcessSocketConnection(ISocket client, MessageArgs args)
+        {
+            byte[] packageSizeBuffer = new byte[sizeof(long)];
+            int position = client.Receive(packageSizeBuffer);
+
+            long requestSize = BitConverter.ToInt64(packageSizeBuffer, 0);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                await ms.WriteAsync(packageSizeBuffer, 0, position);
+                position = 0;
+                while (position != requestSize)
+                {
+                    byte[] package = new byte[client.BufferSize];
+
+                    int received = client.Receive(package);
+
+                    await ms.WriteAsync(package, 0, received);
+                    position += received;
+                }
+
+                // send confirmation
+                byte[] statusBuffer = BitConverter.GetBytes(ms.Length);
+                client.Send(statusBuffer);
+
+                args.RequestBytes = ms.ToArray();
+            }
+        }
+    }
+}
--- Src/XGain/Processing/IProcessor.cs
+++ Src/XGain/Processing/IProcessor.cs
@@ -1,9 +1,10 @@
-﻿using XGain.Sockets;
+﻿using System.Threading.Tasks;
+using XGain.Sockets;
 
 namespace XGain.Processing
 {
     public interface IProcessor<in T> where T : MessageArgs
     {
-        void ProcessSocketConnection(ISocket socket, T args);
+        Task ProcessSocketConnection(ISocket socket, T args);
     }
 }
