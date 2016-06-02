8bfc758730cf845032c3f03d3a50e16eef70527c
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-06-02 20:09:02 +0200

    Removed unused ctor

--- Src/XGain/EventArgs/MessageArgs.cs
+++ Src/XGain/EventArgs/MessageArgs.cs
@@ -5,18 +5,12 @@ namespace XGain
 {
     public class MessageArgs : EventArgs
     {
-        public MessageArgs()
-        {
-        }
-
-        public MessageArgs(ISocket client, object userToken, byte[] requestBytes)
+        public MessageArgs(ISocket client)
         {
             Client = client;
-            UserToken = userToken;
-            RequestBytes = requestBytes;
         }
 
-        public ISocket Client { get; set; }
+        public ISocket Client { get; }
         public object UserToken { get; set; }
         public byte[] RequestBytes { get; set; }
     }
