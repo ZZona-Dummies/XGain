425c69e264b546854a0f40cbca46cbd3be2a2dbb
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-06-04 14:50:55 +0200

    Basic version of Benchmark

---
+++ Src/Benchmark/XGain.Benchmark/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
+
+namespace XGain.Benchmark
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var summary = BenchmarkRunner.Run<RecivingTime>();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Src/Benchmark/XGain.Benchmark/RecivingTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace XGain.Benchmark
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class RecivingTime
+    {
+        private const int N = 10000;
+        private readonly byte[] data;
+
+        public RecivingTime()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public void DoWork()
+        {
+            const int port = 5000;
+
+            IServer server = new XGainServer(IPAddress.Any, port);
+            Task ServerWorker = server.Start(CancellationToken.None, 1);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var client = new TcpClient();
+                client.ConnectAsync(IPAddress.Parse("192.168.43.75"), port).Wait();
+
+                client.GetStream().WriteAsync(data, 0, data.Length).Wait();
+            }
+        }
+    }
+}
