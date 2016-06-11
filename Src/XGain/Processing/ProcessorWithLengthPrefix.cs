4f6dd78a67962dad209aa6ce44a8e8b7557c891b
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-06-11 10:59:12 +0200

    System.Buffer instead of new byte array

--- Src/XGain/Processing/ProcessorWithLengthPrefix.cs
+++ Src/XGain/Processing/ProcessorWithLengthPrefix.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.IO;
 using System.Threading.Tasks;
 using XGain.Sockets;
@@ -9,7 +10,7 @@ namespace XGain.Processing
     {
         public async Task ProcessSocketConnection(ISocket client, MessageArgs args)
         {
-            byte[] packageSizeBuffer = new byte[sizeof(long)];
+            byte[] packageSizeBuffer = ArrayPool<byte>.Shared.Rent(sizeof(long));
             int position = client.Receive(packageSizeBuffer);
 
             long requestSize = BitConverter.ToInt64(packageSizeBuffer, 0);
@@ -20,12 +21,14 @@ namespace XGain.Processing
                 position = 0;
                 while (position != requestSize)
                 {
-                    byte[] package = new byte[client.BufferSize];
+                    byte[] package = ArrayPool<byte>.Shared.Rent(client.BufferSize);
 
                     int received = client.Receive(package);
 
                     await ms.WriteAsync(package, 0, received);
                     position += received;
+
+                    ArrayPool<byte>.Shared.Return(package, true);
                 }
 
                 // send confirmation
@@ -34,6 +37,8 @@ namespace XGain.Processing
 
                 args.RequestBytes = ms.ToArray();
             }
+
+            ArrayPool<byte>.Shared.Return(packageSizeBuffer, true);
         }
     }
 }
