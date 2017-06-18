4cfd2ab0f3b75036237bb82251d67f0a8f375a82
Lukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-06-18 19:42:21 +0200

    Added sample client

---
+++ Sample/SampleClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using XGain;
+
+namespace SampleClient
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StartAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task StartAsync()
+        {
+            var client = new XGainClient(IPAddress.Loopback, 5000);
+            var bytes = Encoding.UTF8.GetBytes("hello world");
+            while (true)
+            {
+                await client.SendAsync(bytes).ConfigureAwait(false);
+            }
+        }
+    }
+}
