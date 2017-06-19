102386f8cd44c9c17a462c66d4b04ca3bc09f9c4
Lukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-06-19 09:10:26 +0200

    Added benchmark

---
+++ Src/Benchmark/Configs/WithAnalyzers.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Order;
+
+namespace Benchmark.Configs
+{
+    [OrderProvider(SummaryOrderPolicy.FastestToSlowest)]
+    public class WithAnalyzers : ManualConfig
+    {
+        public WithAnalyzers()
+        {
+            Add(StatisticColumn.Mean);
+            Add(StatisticColumn.StdErr);
+            Add(StatisticColumn.StdDev);
+            Add(StatisticColumn.Median);
+            Add(CsvMeasurementsExporter.Default);
+            Add(RPlotExporter.Default);
+            Add(MemoryDiagnoser.Default);
+        }
+    }
+}
---
+++ Src/Benchmark/EndToEnd.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using XGain;
+
+namespace Benchmark
+{
+    public class EndToEnd
+    {
+        [Params(128, 1024, 2048)]
+        public int Size { get; set; }
+
+        [Params(1, 10, 50)]
+        public int Iterations { get; set; }
+
+        private readonly IPAddress address = IPAddress.Loopback;
+        private const int port = 5000;
+        private byte[] package;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            package = new byte[Size];
+        }
+
+        [Benchmark]
+        public async Task Start()
+        {
+            using (var server = new XGainServer(address, port))
+            {
+                server.OnNewMessage += (sender, args) => { };
+                server.Start();
+
+                var client = new XGainClient(address, port);
+                for (int i = 0; i < Iterations; i++)
+                {
+                    await client.SendAsync(package);
+                }
+            }
+        }
+    }
+}
